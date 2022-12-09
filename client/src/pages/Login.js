58f1f7f288d15b450c85b2014a33d859195edc98
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-12-09 12:14:15 -0500

    created login page with conditional rendering

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,46 @@
-const Login = () => {
-  return <div>Login</div>
+import LoginForm from '../components/LoginForm'
+import RegisterForm from '../components/RegisterForm'
+import { userState } from 'react'
+
+const Login = ({ setUser }) => {
+  const [isVisible, setIsVisible] = userState(true)
+
+  const handleLogin = () => {
+    setIsVisible(false)
+  }
+
+  const handleRegister = () => {
+    setIsVisible(true)
+  }
+
+  return (
+    <div className="home">
+      <h1 className="homeHeader">PRINCESS TIME!!@</h1>
+      <div className="loginRegister">
+        {isVisible ? (
+          <div className="homeLogin">
+            <LoginForm setUser={setUser} />
+            <div className="registerInstead">
+              <h3 className="homeOption">Need to create an account?</h3>
+            </div>
+            <button className="homeButton" onClick={handleLogin}>
+              Create
+            </button>
+          </div>
+        ) : (
+          <div className="homeRegister">
+            <RegisterForm setIsVisible={setIsVisible} />
+            <div className="loginInstead">
+              <h3 className="homeOption">Already have an account?</h3>
+            </div>
+            <button className="homeButton" onClick={handleRegister}>
+              Create
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Login
