342634da1bbba59fa0eba214a165988bba17376e
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-12-09 10:44:48 -0500

    created components and pages

--- princess-generator/src/App.js
+++ princess-generator/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Create, Gallery, Login } from './pages'
+import { CreateForm, LoginForm, Nav, RegisterForm } from './components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
