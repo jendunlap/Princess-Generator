04f086152bc99bac4a8289f6fcec9587e3628adb
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-12-08 21:20:29 -0500

    princess controllers and routes work

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => res.send('Root!!'))
 
 router.get('/princesses', controllers.getAllPrincesses)
 
+router.get('/princesses/:id', controllers.getPrincessById)
+
+router.put('/princesses/:id', controllers.updatePrincess)
+
+router.delete('/princesses/:id', controllers.deletePrincess)
+
+router.post('/princesses', controllers.createPrincess)
+
 module.exports = router
