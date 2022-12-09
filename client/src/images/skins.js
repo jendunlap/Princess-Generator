8dc62541104d27994e786e0b8bdfe039067dad09
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-12-09 18:42:47 -0500

    successfully generated princess skin selector

---
+++ client/src/images/skins.js
@@ -0,0 +1,19 @@
+const skins = [
+  {
+    name: 'Ariel',
+    color: 'https://i.imgur.com/wjqV9Iw.jpg',
+    url: 'https://i.imgur.com/HFj9sAV.png'
+  },
+  {
+    name: 'Mulan',
+    color: 'https://i.imgur.com/wIlz34M.jpg',
+    url: 'https://i.imgur.com/mrcU9TD.png'
+  },
+  {
+    name: 'Tiana',
+    color: 'https://i.imgur.com/CsB00mV.jpg',
+    url: 'https://i.imgur.com/3tD8Aaw.png'
+  }
+]
+
+export default skins
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,5 +1,74 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios'
+import skins from '../images/skins.js'
+
 const Create = () => {
-  return <div>Create</div>
+  console.log(skins)
+  let navigate = useNavigate()
+
+  const initialState = {
+    skin: 'https://i.imgur.com/HFj9sAV.png'
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const [selectingSkin, setSelectingSkin] = useState(false)
+
+  const selectSkin = (skins) => {
+    let tempState = { ...formState, skin: skins.url }
+    setFormState(tempState)
+    setSelectingSkin(false)
+  }
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value })
+  }
+
+  return (
+    <div className="createPage">
+      <form className="createForm">
+        <label className="skinDiv formLabel" htmlFor="selectSkin">
+          Skin Tone
+        </label>
+        {selectingSkin ? (
+          <div className="skinMap">
+            {skins.map((skin, index) => (
+              <img
+                className="mappedskin"
+                key={index}
+                src={skin.color}
+                alt={skin.name}
+                onClick={() => selectSkin(skin)}
+              />
+            ))}
+          </div>
+        ) : (
+          <div
+            className="selectSkinButton"
+            onClick={() => setSelectingSkin(true)}
+          >
+            {skins.map((skin, index) => (
+              <img
+                className="mappedskin"
+                key={index}
+                src={skin.color}
+                alt={skin.name}
+                onClick={() => selectSkin(skin)}
+              />
+            ))}
+          </div>
+        )}
+      </form>
+      <div className="princessCreation">
+        <img className="princessCreationComponent" src={formState.skin} />
+        <img
+          className="princessCreationComponent"
+          src="https://i.imgur.com/KQ6ib8I.png"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Create
