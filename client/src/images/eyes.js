a9f97415cef640434f7660f543a7e9f537710f1a
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-12-09 19:48:48 -0500

    selecting eyes works

---
+++ client/src/images/eyes.js
@@ -0,0 +1,24 @@
+const eyes = [
+  {
+    name: 'Cinderella',
+    shape: 'https://i.imgur.com/RPZBLvU.jpg',
+    url: 'https://i.imgur.com/KQ6ib8I.png'
+  },
+  {
+    name: 'Ariel',
+    shape: 'https://i.imgur.com/lBeb0Jz.jpg',
+    url: 'https://i.imgur.com/BiGJdaN.png'
+  },
+  {
+    name: 'Mulan',
+    shape: 'https://i.imgur.com/jVHZHNZ.jpg',
+    url: 'https://i.imgur.com/UAEbCRh.png'
+  },
+  {
+    name: 'Tiana',
+    shape: 'https://i.imgur.com/neyDuap.jpg',
+    url: 'https://i.imgur.com/Wv1y1GZ.png'
+  }
+]
+
+export default eyes
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -2,32 +2,44 @@ import { useEffect, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import axios from 'axios'
 import skins from '../images/skins.js'
+import eyes from '../images/eyes.js'
 
 const Create = () => {
   console.log(skins)
-  let navigate = useNavigate()
+  // let navigate = useNavigate()
 
   const initialState = {
-    skin: 'https://i.imgur.com/HFj9sAV.png'
+    skin: 'https://i.imgur.com/HFj9sAV.png',
+    eyes: 'https://i.imgur.com/KQ6ib8I.png'
   }
 
   const [formState, setFormState] = useState(initialState)
 
   const [selectingSkin, setSelectingSkin] = useState(false)
+  const [selectingEyes, setSelectingEyes] = useState(false)
+  const [selectedEyes, setSelectedEyes] = useState(false)
 
   const selectSkin = (skins) => {
     let tempState = { ...formState, skin: skins.url }
     setFormState(tempState)
     setSelectingSkin(false)
   }
-
-  const handleChange = (e) => {
-    setFormState({ ...formState, [e.target.id]: e.target.value })
+  const selectEyes = (eyes) => {
+    let tempState = { ...formState, eyes: eyes.url }
+    setSelectedEyes(eyes.url)
+    setFormState(tempState)
+    setSelectingEyes(false)
   }
+
+  // const handleChange = (e) => {
+  //   setFormState({ ...formState, [e.target.id]: e.target.value })
+  // }
 
   return (
     <div className="createPage">
       <form className="createForm">
+        {/* SKIN SELECTION */}
+
         <label className="skinDiv formLabel" htmlFor="selectSkin">
           Skin Tone
         </label>
@@ -35,7 +47,7 @@ const Create = () => {
           <div className="skinMap">
             {skins.map((skin, index) => (
               <img
-                className="mappedskin"
+                className="mappedSkin"
                 key={index}
                 src={skin.color}
                 alt={skin.name}
@@ -50,7 +62,7 @@ const Create = () => {
           >
             {skins.map((skin, index) => (
               <img
-                className="mappedskin"
+                className="mappedSkin"
                 key={index}
                 src={skin.color}
                 alt={skin.name}
@@ -59,13 +71,50 @@ const Create = () => {
             ))}
           </div>
         )}
+        {/* EYE SELECTION */}
+
+        <label className="eyesDiv formLabel" htmlFor="selectEyes">
+          Eye Shape
+        </label>
+        {selectingEyes ? (
+          <div className="eyeMap">
+            {eyes.map((eye, index) => (
+              <img
+                className="mappedEyes"
+                key={index}
+                src={eye.shape}
+                alt={eye.name}
+                onClick={() => selectEyes(eye)}
+              />
+            ))}
+          </div>
+        ) : (
+          <div
+            className="selectEyesButton"
+            onClick={() => setSelectingEyes(true)}
+          >
+            {eyes.map((eye, index) => (
+              <img
+                className="mappedEyes"
+                key={index}
+                src={eye.shape}
+                alt={eye.name}
+                onClick={() => selectEyes(eye)}
+              />
+            ))}
+          </div>
+        )}
       </form>
       <div className="princessCreation">
         <img className="princessCreationComponent" src={formState.skin} />
-        <img
-          className="princessCreationComponent"
-          src="https://i.imgur.com/KQ6ib8I.png"
-        />
+        {selectedEyes ? (
+          <img className="princessCreationComponent" src={selectedEyes} />
+        ) : (
+          <img
+            className="princessCreationComponent"
+            src="https://i.imgur.com/KQ6ib8I.png"
+          />
+        )}
       </div>
     </div>
   )
