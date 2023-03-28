5d858702af5c5f0a997d47b89a9e751d0bfa04f0
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-03-27 21:42:11 -0400

    two more necklaces

--- client/src/images/necklaces.js
+++ client/src/images/necklaces.js
@@ -15,6 +15,16 @@ const necklaces = [
     url: 'https://i.imgur.com/IgKeE2E.png'
   },
   {
+    name: 'Moana1',
+    shape: 'https://i.imgur.com/OGFVtjk.png',
+    url: 'https://i.imgur.com/bAp3NHa.png'
+  },
+  {
+    name: 'Moana2',
+    shape: 'https://i.imgur.com/Qlopc0L.png',
+    url: 'https://i.imgur.com/I6iguyq.png'
+  },
+  {
     name: 'None',
     shape: 'https://i.imgur.com/J8cnpui.png',
     url: 'https://i.imgur.com/J8cnpui.png'
